74d440c3c21d1e86d2f6e30c7512333b6fd84dd5
Bunno Sousa Monte <brunnosousa.bs97@gmail.com>
2018-08-27 20:01:44 -0300

    Primeiro Programa Spring

---
+++ spring/fvs/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package fvs;
+
+public class PrimeiroPrograma {
+
+	public PrimeiroPrograma() {
+	
+	}
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World!");
+		
+
+	}
+
+}
