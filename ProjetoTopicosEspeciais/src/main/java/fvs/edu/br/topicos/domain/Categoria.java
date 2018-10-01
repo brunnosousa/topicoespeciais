3479125375150d7da1dda56a30ddb4b7cc086dd8
Brunno Sousa Monte <brunnosousa.bs97@gmail.com>
2018-10-01 20:02:11 -0300

    Implementação do Dominio Categoria

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Categoria.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Categoria.java
@@ -1,11 +1,15 @@
 package fvs.edu.br.topicos.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 @Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -15,6 +19,8 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy="categorias")
+	List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria( ) {
 		
