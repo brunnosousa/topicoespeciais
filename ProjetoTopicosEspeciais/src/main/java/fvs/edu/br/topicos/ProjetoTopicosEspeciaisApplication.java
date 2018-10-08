ad9bc1a4f261054b8548d4931f564d8bc3ca12fb
Brunno Sousa Monte <brunnosousa.bs97@gmail.com>
2018-10-08 20:01:12 -0300

    Implementação da cidade e estado

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/ProjetoTopicosEspeciaisApplication.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/ProjetoTopicosEspeciaisApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import fvs.edu.br.topicos.domain.Categoria;
+import fvs.edu.br.topicos.domain.Cidade;
+import fvs.edu.br.topicos.domain.Estado;
 import fvs.edu.br.topicos.domain.Produto;
 import fvs.edu.br.topicos.repositories.CategoriaRepository;
+import fvs.edu.br.topicos.repositories.CidadeRepository;
+import fvs.edu.br.topicos.repositories.EstadoRepository;
 import fvs.edu.br.topicos.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,8 +22,16 @@ public class ProjetoTopicosEspeciaisApplication implements CommandLineRunner{
 	@Autowired
 	CategoriaRepository catRepository;
 	
+	@Autowired
 	ProdutoRepository prodRepository;
 	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoTopicosEspeciaisApplication.class, args);
 	}
@@ -43,6 +55,19 @@ public class ProjetoTopicosEspeciaisApplication implements CommandLineRunner{
 		
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
 		prodRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "minas gerais");
+		Estado est2 = new Estado(null, "ceara");
+		
+		Cidade c1 = new Cidade(null, "uberladia", est1);
+		Cidade c2 = new Cidade(null, "ico", est2);
+		Cidade c3 = new Cidade(null, "sobral", est2);
+		
+		est1.getCidade().addAll(Arrays.asList(c1));
+		est2.getCidade().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 	
